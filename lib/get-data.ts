1f1e343df0e83a0a94f5d918c35ba62139ac8b22
Vencer Torres <vencer.torres@gmail.com>
2023-02-14 11:52:36 +0800

    fix: handle error when response is null

--- lib/get-data.ts
+++ lib/get-data.ts
@@ -5,6 +5,10 @@ export async function getData(id: number) {
   const res = await fetch(`${API_URL}/item/${id}.json`)
   const data = await res.json()
 
+  if (!data) {
+    throw new Error('Item not found')
+  }
+
   return {
     ...data,
     time: format(data.time * 1000),
