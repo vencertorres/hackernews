10870c84365b5933a9bca04fe45d45cb47c7a2e3
Vencer Torres <vencer.torres@gmail.com>
2023-02-15 13:22:30 +0800

    feat: logo

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,10 +6,11 @@ export default function Navigation() {
   const router = useRouter()
 
   return (
-    <nav className="space-x-4 p-4">
+    <nav className="flex items-center gap-4 p-4">
+      <span className="mr-auto bg-orange-400 p-1 text-neutral-50">HN</span>
       {Object.keys(lists).map((list) => (
         <Link
-          className="inline-block capitalize underline hover:text-slate-500 data-[active=true]:font-medium data-[active=true]:text-orange-500 data-[active=true]:no-underline"
+          className="inline-block capitalize underline hover:text-slate-500 data-[active=true]:font-medium data-[active=true]:text-orange-400 data-[active=true]:no-underline"
           data-active={router.query.list === list}
           key={list}
           href={`/${list}?p=1`}
