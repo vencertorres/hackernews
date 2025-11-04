5d91755d2fbe070a6c53be63a02b102ffdae07b4
Vencer Torres <vencer.torres@gmail.com>
2025-11-04 12:22:22 +0800

    fix: active link
    Active link only performs an exact match (i.e. /top/1 will not be active for /top/2). By passing a pattern to `useRoute` we ensure that any parameter gets matched.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,21 @@
 import type { ReactNode } from "react";
-import { Link } from "wouter";
+import { Link, useRoute } from "wouter";
 import styles from "./Navigation.module.css";
 
 function NavigationItem({
   path,
+  pattern,
   children,
 }: {
   path: string;
+  pattern: string;
   children: ReactNode;
 }) {
+  const [isActive] = useRoute(pattern);
+
   return (
     <li className={styles.listItem}>
-      <Link
-        href={path}
-        className={(active) => (active ? styles.active : styles.link)}
-      >
+      <Link href={path} className={isActive ? styles.active : styles.link}>
         {children}
       </Link>
     </li>
@@ -25,11 +26,21 @@ export default function Navigation() {
   return (
     <nav className={styles.nav}>
       <ul className={styles.list}>
-        <NavigationItem path="/top/1">Top</NavigationItem>
-        <NavigationItem path="/new/1">New</NavigationItem>
-        <NavigationItem path="/ask/1">Ask</NavigationItem>
-        <NavigationItem path="/show/1">Show</NavigationItem>
-        <NavigationItem path="/jobs/1">Jobs</NavigationItem>
+        <NavigationItem path="/top/1" pattern="/top/:page">
+          Top
+        </NavigationItem>
+        <NavigationItem path="/new/1" pattern="/new/:page">
+          New
+        </NavigationItem>
+        <NavigationItem path="/ask/1" pattern="/ask/:page">
+          Ask
+        </NavigationItem>
+        <NavigationItem path="/show/1" pattern="/show/:page">
+          Show
+        </NavigationItem>
+        <NavigationItem path="/jobs/1" pattern="/jobs/:page">
+          Jobs
+        </NavigationItem>
       </ul>
     </nav>
   );
